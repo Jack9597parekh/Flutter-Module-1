e49eedf05ebc29f4286192f6c90a0ff00de5570f
Jack9597parekh <134036740+Jack9597parekh@users.noreply.github.com>
2023-12-08 19:02:32 +0530

    Add files via upload

---
+++ Q1.dart
@@ -0,0 +1,8 @@
+void main(){
+  print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
+  print('*            My Name is Jay Parekh             *');
+  print('*        My Birthdate is 6th February 1997          *');
+  print('*               I am 27 years Old.                  *');
+  print('*             My address is  Surat                  *');
+  print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
+}
